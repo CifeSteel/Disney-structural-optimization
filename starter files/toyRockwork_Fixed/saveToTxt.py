d394dbecd9c0b6b6e26804f41616242f6670063c
Filippo Ranalli <Filippo Ranalli>
2016-08-19 17:27:13 -0700

    debugging

--- starter files/toyRockwork_Fixed/saveToTxt.py
+++ starter files/toyRockwork_Fixed/saveToTxt.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-sapIMem = pd.read_csv("SAP_I_Node.csv")
+item1 = pd.read_csv("SAP_I_Node.csv")
+item2 = pd.read_csv("SAP_I_Member.csv")
+item3 = pd.read_csv("node_geometry.csv")
+item4 = pd.read_csv("member_geometry.csv")
 
-sapIMem.to_csv("SAP_I_Node.txt", index = False)
+item1.to_csv("SAP_I_Node.txt", index = False)
+item2.to_csv("SAP_I_Member.txt", index = False)
+item3.to_csv("node_geometry.txt", index = False)
+item4.to_csv("member_geometry.txt", index = False)
