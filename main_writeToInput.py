f84b4b854b8f0444ccda6f74270a70891c508158
Filippo Ranalli <Filippo Ranalli>
2016-09-04 09:16:11 -0700

    updated optimizer

--- main_writeToInput.py
+++ main_writeToInput.py
@@ -11,7 +11,10 @@ sizes = memberSizes["cross_section"]
 for i in range(0, len(memberIDs)):
 	sapIMember.loc[sapIMember["member_ID"] == memberIDs[i], "size"] = sizes[i]
 
-for i in range(0, len(sapIMember["member_ID"])):
-	sapIMember.ix[i, "add_back?"] = False
+for i in range(0, len(memberIDs)):
+	sapIMember.loc[sapIMember["member_ID"] == memberIDs[i],"add_back?"] = False
+
+#for i in range(0, len(sapIMember["member_ID"])):
+	#sapIMember.ix[i,"add_back?"] = False
 
 sapIMember.to_csv("SAP_I_Member.txt", index = False)
